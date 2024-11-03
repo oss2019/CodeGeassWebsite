a71aa873f6739017d0a7f2768634555bbd9384ea
Harsh Chauhan <harsh2005.hc@gmail.com>
2024-11-04 02:26:43 +0530

    fetching webdev member data

--- pages/admin/members.tsx
+++ pages/admin/members.tsx
@@ -1,40 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react'
-import axios from 'axios'
-import { Tabs, type TabsRef } from 'flowbite-react'
-import Loading from '@/components/Loading/Loading'
-import InfoTable from '@/components/Table/InfoTable'
-import styles from './../../app/members/page.module.css'
-import { FOSS } from '@/types/Members/foss.types'
-import { CP } from '@/types/Members/cp.types'
-import { GD } from '@/types/Members/gd.types'
-import { members } from '@/types/Members/members.types'
-import { SiBmcsoftware, SiCodeforces } from 'react-icons/si'
-import { IoGameController } from 'react-icons/io5'
-import { IoIosPeople } from 'react-icons/io'
-import Modal from 'react-modal'
-import Alert from '@mui/material/Alert'
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Tabs, type TabsRef } from 'flowbite-react';
+import Loading from '@/components/Loading/Loading';
+import InfoTable from '@/components/Table/InfoTable';
+import styles from './../../app/members/page.module.css';
+import { FOSS } from '@/types/Members/foss.types';
+import { CP } from '@/types/Members/cp.types';
+import { GD } from '@/types/Members/gd.types';
+import { WebDev } from '@/types/Members/webdev.types';
+import { members } from '@/types/Members/members.types';
+import { SiBmcsoftware, SiCodeforces } from 'react-icons/si';
+import { IoGameController } from 'react-icons/io5';
+import { IoIosPeople } from 'react-icons/io';
+import Modal from 'react-modal';
+import Alert from '@mui/material/Alert';
 
-const MEMBERS_REVALIDATION_TIME = 60 * 60 * 12
+const MEMBERS_REVALIDATION_TIME = 60 * 60 * 12;
 
 interface FormData {
-  name: string
-  roll_number: string
-  wing: string
-  github_id?: string
-  role?: string
-  leetCode_id?: string
-  codeForces_id?: string
+  name: string;
+  roll_number: string;
+  wing: string;
+  github_id?: string;
+  role?: string;
+  leetCode_id?: string;
+  codeForces_id?: string;
 }
-const Page = () => {
-  const [memArr, setMemArr] = useState<string[][]>([[]])
-  const [cpArr, setCpArr] = useState<string[][]>([[]])
-  const [gdArr, setGdArr] = useState<string[][]>([[]])
-  const [fossArr, SetFossArr] = useState<string[][]>([[]])
-  const [activeTab, setActiveTab] = useState<number>(0)
-  const [error, setError] = useState<string | null>(null)
-  const tabsRef = useRef<TabsRef>(null)
+
+const MembersPage = () => {
+  const [memArr, setMemArr] = useState<string[][]>([[]]);
+  const [cpArr, setCpArr] = useState<string[][]>([[]]);
+  const [gdArr, setGdArr] = useState<string[][]>([[]]);
+  const [fossArr, SetFossArr] = useState<string[][]>([[]]);
+  const [webdevArr, setWebdevArr] = useState<string[][]>([[]]);
+  const [activeTab, setActiveTab] = useState<number>(0);
+  const [error, setError] = useState<string | null>(null);
+  const tabsRef = useRef<TabsRef>(null);
   const props = { setActiveTab, tabsRef }
-  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     name: '',
     roll_number: '',
@@ -43,61 +47,59 @@ const Page = () => {
     role: 'DEVELOPER',
     leetCode_id: '',
     codeForces_id: '',
-  })
+  });
 
   const handleOpen = () => {
-    setIsModalVisible(true)
-  }
+    setIsModalVisible(true);
+  };
 
   const handleClose = () => {
-    setIsModalVisible(false)
-  }
+    setIsModalVisible(false);
+  };
 
   const handleFormChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
-    })
-    setError(null)
-  }
+    });
+    setError(null);
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    if (formData.name == '') {
-      setError('Name should not be empty')
-    } else if (formData.roll_number == '') {
-      // add regex of roll number here
-      setError('Roll Number should not be empty')
-    } else if (formData.wing === 'FOSS' && formData.github_id == '') {
-      setError('Github Id should not be empty')
+    e.preventDefault();
+    if (formData.name === '') {
+      setError('Name should not be empty');
+    } else if (formData.roll_number === '') {
+      setError('Roll Number should not be empty');
+    } else if (formData.wing === 'FOSS' && formData.github_id === '') {
+      setError('Github Id should not be empty');
     } else if (
       formData.wing === 'CP' &&
-      formData.codeForces_id == '' &&
-      formData.leetCode_id == ''
+      formData.codeForces_id === '' &&
+      formData.leetCode_id === ''
     ) {
-      setError('Provide details for atleast 1 CP handle')
-    } else if (error == null) {
+      setError('Provide details for at least 1 CP handle');
+    } else if (error === null) {
       try {
-        const response = await axios.post('/api/admin/members', formData)
-        // BUG : adding FOSS members with same github id -->> members table is updated, but it should not be
-        //     : similarly for CP's codeforces and leetcode attributes
-        console.log(response.data)
-        fetchDataMembers()
-        fetchCpData()
-        fetchDataGameDev()
-        fetchDataFoss()
-        console.log('Member added successfully')
+        const response = await axios.post('/api/admin/members', formData);
+        console.log(response.data);
+        fetchDataMembers();
+        fetchCpData();
+        fetchDataGameDev();
+        fetchDataFoss();
+        fetchDataWebDev();
+        console.log('Member added successfully');
       } catch (error: any) {
-        setError(error.response.data.error)
-        console.log('Error adding member:', error)
+        setError(error.response.data.error);
+        console.log('Error adding member:', error);
       } finally {
-        handleClose()
+        handleClose();
       }
     }
-  }
+  };
 
   const fetchDataMembers = async () => {
     const res = await fetch(`/api/fetch/members`, {
@@ -105,18 +107,18 @@ const Page = () => {
         revalidate: MEMBERS_REVALIDATION_TIME,
       },
       method: 'GET',
-    })
+    });
     if (res.status !== 200) {
-      setMemArr([[]])
+      setMemArr([[]]);
     }
-    const data = await res.json()
-    const dict_data: members[] = JSON.parse(data.members)
-    const arr: string[][] = []
+    const data = await res.json();
+    const dict_data: members[] = JSON.parse(data.members);
+    const arr: string[][] = [];
     dict_data.forEach(element => {
-      arr.push(Object.values(element).map(e => e.toString()))
-    })
-    setMemArr(arr)
-  }
+      arr.push(Object.values(element).map(e => e.toString()));
+    });
+    setMemArr(arr);
+  };
 
   const fetchCpData = async () => {
     const res = await fetch(`/api/fetch/cpmembers`, {
@@ -124,38 +126,38 @@ const Page = () => {
         revalidate: MEMBERS_REVALIDATION_TIME,
       },
       method: 'GET',
-    })
+    });
     if (res.status !== 200) {
-      setCpArr([[]])
+      setCpArr([[]]);
     }
-    const data = await res.json()
-    const cf_data: CP[] = JSON.parse(data.codeforces)
-    const lt_data: CP[] = JSON.parse(data.leetcode)
-    const arr: string[][] = []
+    const data = await res.json();
+    const cf_data: CP[] = JSON.parse(data.codeforces);
+    const lt_data: CP[] = JSON.parse(data.leetcode);
+    const arr: string[][] = [];
     cf_data.forEach(element => {
       let new_ele = {
         name: '',
         roll_number: 0,
         handle: '',
-      }
-      new_ele['name'] = element.member.name
-      new_ele['roll_number'] = element.member.roll_number
-      new_ele['handle'] = element.handle
-      arr.push(Object.values(new_ele).map(e => e.toString()))
-    })
+      };
+      new_ele['name'] = element.member.name;
+      new_ele['roll_number'] = element.member.roll_number;
+      new_ele['handle'] = element.handle;
+      arr.push(Object.values(new_ele).map(e => e.toString()));
+    });
     lt_data.forEach(element => {
       let new_ele = {
         name: '',
         roll_number: 0,
         handle: '',
-      }
-      new_ele['name'] = element.member.name
-      new_ele['roll_number'] = element.member.roll_number
-      new_ele['handle'] = element.handle
-      arr.push(Object.values(new_ele).map(e => e.toString()))
-    })
-    setCpArr(arr)
-  }
+      };
+      new_ele['name'] = element.member.name;
+      new_ele['roll_number'] = element.member.roll_number;
+      new_ele['handle'] = element.handle;
+      arr.push(Object.values(new_ele).map(e => e.toString()));
+    });
+    setCpArr(arr);
+  };
 
   const fetchDataGameDev = async () => {
     const res = await fetch(`/api/fetch/gdmembers`, {
@@ -163,26 +165,26 @@ const Page = () => {
         revalidate: MEMBERS_REVALIDATION_TIME,
       },
       method: 'GET',
-    })
+    });
     if (res.status !== 200) {
-      setGdArr([[]])
+      setGdArr([[]]);
     }
-    const data = await res.json()
-    const dict_data: GD[] = JSON.parse(data.gameDevMembers)
-    const arr: string[][] = []
+    const data = await res.json();
+    const dict_data: GD[] = JSON.parse(data.gameDevMembers);
+    const arr: string[][] = [];
     dict_data.forEach(element => {
       let new_ele = {
         name: '',
         roll_number: 0,
         role: '',
-      }
-      new_ele['name'] = element.member.name
-      new_ele['roll_number'] = element.member.roll_number
-      new_ele['role'] = element.role
-      arr.push(Object.values(new_ele).map(e => e.toString()))
-    })
-    setGdArr(arr)
-  }
+      };
+      new_ele['name'] = element.member.name;
+      new_ele['roll_number'] = element.member.roll_number;
+      new_ele['role'] = element.role;
+      arr.push(Object.values(new_ele).map(e => e.toString()));
+    });
+    setGdArr(arr);
+  };
 
   const fetchDataFoss = async () => {
     const res = await fetch(`/api/fetch/fossmembers`, {
@@ -190,38 +192,66 @@ const Page = () => {
         revalidate: MEMBERS_REVALIDATION_TIME,
       },
       method: 'GET',
-    })
+    });
     if (res.status !== 200) {
-      SetFossArr([[]])
+      SetFossArr([[]]);
     }
-    const data = await res.json()
-    const dict_data: FOSS[] = JSON.parse(data.foss)
-    const arr: string[][] = []
+    const data = await res.json();
+    const dict_data: FOSS[] = JSON.parse(data.foss);
+    const arr: string[][] = [];
     dict_data.forEach(element => {
       let new_ele = {
         name: '',
         roll_number: 0,
         github_uname: '',
-      }
-      new_ele['name'] = element.member.name
-      new_ele['roll_number'] = element.member.roll_number
-      new_ele['github_uname'] = element.github_uname
-      arr.push(Object.values(new_ele).map(e => e.toString()))
-    })
-    SetFossArr(arr)
-  }
+      };
+      new_ele['name'] = element.member.name;
+      new_ele['roll_number'] = element.member.roll_number;
+      new_ele['github_uname'] = element.github_uname;
+      arr.push(Object.values(new_ele).map(e => e.toString()));
+    });
+    SetFossArr(arr);
+  };
+
+  const fetchDataWebDev = async () => {
+    const res = await fetch(`/api/fetch/webdevmembers`, {
+      next: {
+        revalidate: MEMBERS_REVALIDATION_TIME,
+      },
+      method: 'GET',
+    });
+    if (res.status !== 200) {
+      setWebdevArr([[]]);
+    }
+    const data = await res.json();
+    const dict_data: WebDev[] = JSON.parse(data.webDevMembers);
+    const arr: string[][] = [];
+    dict_data.forEach(element => {
+      let new_ele = {
+        name: '',
+        roll_number: 0,
+        github_uname: '',
+      };
+      new_ele['name'] = element.member.name;
+      new_ele['roll_number'] = element.member.roll_number;
+      new_ele['github_uname'] = element.github_uname;
+      arr.push(Object.values(new_ele).map(e => e.toString()));
+    });
+    setWebdevArr(arr);
+  };
+
   useEffect(() => {
-    fetchDataMembers()
-    fetchCpData()
-    fetchDataGameDev()
-    fetchDataFoss()
-  }, [])
-
-  const headings_mem: string[] = ['Name', 'Roll Number', 'Wing', '']
-  const headings_foss: string[] = ['Name', 'Roll Number', 'GitHub ID']
-  const headings_cp: string[] = ['Name', 'Roll Number', 'handle']
-  const headings_gd: string[] = ['Name', 'Roll Number', 'Role']
-
+    fetchDataMembers();
+    fetchCpData();
+    fetchDataGameDev();
+    fetchDataFoss();
+    fetchDataWebDev();
+  }, []);
+const headings_mem: string[] = ['Name', 'Roll Number', 'Wing', '']
+const headings_foss: string[] = ['Name', 'Roll Number', 'GitHub ID']
+const headings_cp: string[] = ['Name', 'Roll Number', 'handle']
+const headings_gd: string[] = ['Name', 'Roll Number', 'Role']
+const headings_webdev: string[] = ['Name', 'Roll Number', 'GitHub ID']
   return (
     <>
       {error ? (
@@ -232,8 +262,7 @@ const Page = () => {
             setError(null)
           }}
         >
-          {JSON.stringify(error, null, 2).replace(/"/g, '')}{' '}
-          {/* Remove double quotes */}
+          {JSON.stringify(error, null, 2).replace(/"/g, '')}
         </Alert>
       ) : null}
 
@@ -243,7 +272,6 @@ const Page = () => {
           onRequestClose={handleClose}
           shouldCloseOnOverlayClick={true}
           style={{
-            // change to separate css file
             overlay: {
               zIndex: '100',
             },
@@ -282,6 +310,7 @@ const Page = () => {
                 <option value='FOSS'>FOSS</option>
                 <option value='GAME_DEV'>Game Dev</option>
                 <option value='CP'>Competitive Programming</option>
+                <option value='WEB_DEV'>Web Development</option>
                 <option value='NETWORKING'>Networking</option>
                 <option value='CYBERSECURITY'>Cyber Security</option>
               </select>
@@ -309,11 +338,11 @@ const Page = () => {
                     value={formData.role}
                     onChange={handleFormChange}
                   >
-                    <option value=' DEVELOPER'>Developer</option>
-                    <option value='ASSET_DES'>asset des</option>
-                    <option value='MUSIC_COMP'>music comp</option>
-                    <option value='STORY_WRITING'>story writing</option>
-                    <option value='ENV_DES'>env des</option>
+                    <option value='DEVELOPER'>Developer</option>
+                    <option value='ASSET_DES'>Asset Designer</option>
+                    <option value='MUSIC_COMP'>Music Composer</option>
+                    <option value='STORY_WRITING'>Story Writer</option>
+                    <option value='ENV_DES'>Environment Designer</option>
                   </select>
                 </label>
               </div>
@@ -336,6 +365,19 @@ const Page = () => {
                     type='text'
                     name='codeForces_id'
                     value={formData.codeForces_id}
+                    onChange={handleFormChange}
+                  />
+                </label>
+              </div>
+            )}
+            {formData.wing === 'WEB_DEV' && (
+              <div>
+                <label>
+                  GitHub Profile:
+                  <input
+                    type='text'
+                    name='github_id'
+                    value={formData.github_id}
                     onChange={handleFormChange}
                   />
                 </label>
@@ -406,12 +448,25 @@ const Page = () => {
               <Loading />
             )}
           </Tabs.Item>
+          <Tabs.Item title='Web Development' icon={SiBmcsoftware}>
+            {webdevArr ? (
+              <InfoTable
+                headings={headings_webdev}
+                row_data={webdevArr}
+                table_heading='Web Development'
+                setRowData={null}
+              />
+            ) : (
+              <Loading />
+            )}
+          </Tabs.Item>
         </Tabs.Group>
       </div>
     </>
-  )
+  );
 }
 
-Page.requireAuth = true // Required Authentication
 
-export default Page
+MembersPage.requireAuth = true // Required Authentication
+
+export default MembersPage
