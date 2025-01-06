30578783e7f13dafa83bbe8e13daaefbb1150e2b
CodingClub <codingclub@iitdh.ac.in>
2025-01-06 13:39:03 +0530

    Fixed Broken Links /members/faq

--- components/Members/CTA.tsx
+++ components/Members/CTA.tsx
@@ -293,7 +293,7 @@ const CTA = () => {
 
             <div className='flex flex-col mt-8 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4'>
               <Link
-                href='/members/faq'
+                href='/faq'
                 target='_top'
                 className='inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center border rounded-lg focus:ring-4 text-white border-gray-600 hover:bg-gray-700 focus:ring-gray-600'
               >
